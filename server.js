948d2d5edf4e4ed42c7e1e2fe6baf9696abdfdc2
dev0ps221 <seybaxscorpion@gmail.com>
2022-01-22 14:05:01 +0000

    alright , let's make the data move

--- server.js
+++ server.js
@@ -7,13 +7,34 @@ const server	=	http.createServer(app)
 const io 		=	sio()
 const port		=	process.env.PORT || 8000
 const corepath	=	path.join(__dirname,'core/')
+const viewspath	=	path.join(__dirname,'webserver/views')
+const assetspath	=	path.join(__dirname,'webserver/assets')
 const ftipi		=   new new require(path.join(corepath,'ftipi'))
+const fs = require('fs')
 
 //webserver
 function startServer(cb=()=>{}){
 	io.listen(server)
 
+	function doRender(page){
 
+		return fs.readFileSync(path.join(viewspath,'index.html')).toString().replace(
+			'#body',`
+				${fs.readFileSync(path.join(viewspath,'topbar.html'))}
+				${fs.readFileSync(path.join(viewspath,`${page}.html`))}
+			`
+		)
+
+	}
+
+	app.get(
+		'/',(req,res)=>{
+			res.send(doRender(
+				'servers'
+			))
+		}
+
+	)
 
 	server.listen(port,serverStarted)
 }
